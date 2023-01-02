0a9862d571fd06ccf6ed2100a563aa476ef3d2a8
Sem Schilder <sem@tropical.email>
2023-01-02 11:50:35 +0100

    Add token refresh logic

--- src/Authentication/AuthMethod.php
+++ src/Authentication/AuthMethod.php
@@ -12,4 +12,8 @@ interface AuthMethod
     public function isExpired(DateTimeInterface $now = new DateTimeImmutable()): bool;
 
     public function updateRequest(RequestInterface $request): RequestInterface;
+
+    public function getToken(): mixed;
+
+    public function setToken(mixed $token): self;
 }
---
+++ src/Authentication/ExceptionRefresher.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Xvilo\OVpayApi\Authentication;
+
+use Lcobucci\JWT\Token as TokenInterface;
+use RuntimeException;
+use Throwable;
+
+final class ExceptionRefresher implements Refresher
+{
+    public function __construct(
+        private readonly string     $message = "",
+        private readonly int        $code = 0,
+        private readonly ?Throwable $previous = null
+    ) {
+    }
+
+    public function refresh(mixed $token): never
+    {
+        throw new RuntimeException($this->message, $this->code, $this->previous);
+    }
+}
