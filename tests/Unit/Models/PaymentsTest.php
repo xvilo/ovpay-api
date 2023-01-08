68aeb9574cab7a6052d3bf1a03935eb7cc9270f8
Sem Schilder <sem@tropical.email>
2023-01-08 18:24:10 +0100

    Add payments tests

---
+++ tests/Unit/Models/PaymentsTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xvilo\OVpayApi\Tests\Unit\Models;
+
+use Ramsey\Uuid\Uuid;
+use Xvilo\OVpayApi\Models\Payment;
+use Xvilo\OVpayApi\Models\Payments;
+use Xvilo\OVpayApi\Tests\Unit\TestCase;
+
+final class PaymentsTest extends TestCase
+{
+    public function testAddItem(): void
+    {
+        $payments = new Payments(
+            30,
+            true,
+            []
+        );
+
+        self::assertCount(0, $payments->getItems());
+
+        $uuid = Uuid::uuid4()->toString();
+        $time = new \DateTimeImmutable();
+        $payments->addItem(new Payment(
+            'ABCDEFG1234567',
+            $uuid,
+            '12345',
+            'PAYED',
+            $time,
+            'type',
+            1235,
+            0,
+            'EUR',
+            'EMV',
+            '',
+            false,
+        ));
+        self::assertCount(1, $payments->getItems());
+        self::assertEquals($uuid, $payments->getItems()[0]->getXbot());
+        self::assertEquals($time, $payments->getItems()[0]->getTransactionTimestamp());
+    }
+}
